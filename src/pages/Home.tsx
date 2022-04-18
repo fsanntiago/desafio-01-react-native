60a668943d07b476c0904c41b0d5a859c8f9eaf8
fsanntiago <zerofabricio@hotmail.com>
2022-04-18 12:16:55 -0300

    refactor: confirmation alert to remove task

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -41,7 +41,20 @@ export function Home() {
   }
 
   function handleRemoveTask(id: number) {
-    setTasks((oldTasks) => oldTasks.filter((task) => task.id !== id));
+    Alert.alert(
+      "Remover item",
+      "Tem certeza que você deseja remover esse item?",
+      [
+        {
+          text: "Não",
+        },
+        {
+          text: "Sim",
+          onPress: () =>
+            setTasks((oldTasks) => oldTasks.filter((task) => task.id !== id)),
+        },
+      ]
+    );
   }
 
   return (
